887ca6cc972f51090bd4221ec01c36403e52c64b
OgwoGP <37232618+OgwoGP@users.noreply.github.com>
2026-04-08 08:51:02 +0100

    Create [...all]/+server.ts

---
+++ src/routes/api/auth/[...all]/+server.ts
@@ -0,0 +1,11 @@
+// src/routes/api/auth/[...all]/+server.ts
+import { auth } from '$lib/server/auth';
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async ({ request }) => {
+  return auth.handler(request);
+};
+
+export const POST: RequestHandler = async ({ request }) => {
+  return auth.handler(request);
+};
