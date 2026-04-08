b3e3ac0e00e9ed11a772d78b73e6b612bdc257fa
OgwoGP <37232618+OgwoGP@users.noreply.github.com>
2026-04-08 03:51:31 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -114,8 +114,8 @@ $effect(() => {
   <ShutdownPage />
 {:else if MAINTENANCE_MODE}
   <MaintenancePage />
-{:else if !regionAllowed}
-  <RegionBlockedPage onAllowed={() => regionAllowed = true} />
+{:else if REGION_BLOCKED}
+  <RegionBlockedPage />
 {:else if isSuspended}
   <SuspendedPage email={userEmail} />
 {:else if !ageVerified}
