f22c02c0719c677dd14b924cf4a7df1b93e8c780
OgwoGP <37232618+OgwoGP@users.noreply.github.com>
2026-04-10 03:57:08 +0100

    Delete 0002_cheerful_texas_twister.sql

--- drizzle/0002_cheerful_texas_twister.sql
+++
@@ -1,6 +0,0 @@
-DROP INDEX "reports_location_idx";--> statement-breakpoint
-DROP INDEX "saved_locations_location_idx";--> statement-breakpoint
-DROP INDEX "user_profiles_location_idx";--> statement-breakpoint
-CREATE INDEX "reports_location_idx" ON "reports" USING gist ("location"::text);--> statement-breakpoint
-CREATE INDEX "saved_locations_location_idx" ON "saved_locations" USING gist ("location"::text);--> statement-breakpoint
-CREATE INDEX "user_profiles_location_idx" ON "user_profiles" USING gist ("location"::text);
